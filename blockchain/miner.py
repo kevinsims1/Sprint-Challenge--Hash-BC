f86d62c7b34f3a6312243d3bf4d202c0fd6d1d76
kevinsims1 <kevinsims1@outlook.com>
2020-01-17 13:02:47 -0600

    mvp

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -1,14 +1,9 @@
 import hashlib
 import requests
-
 import sys
-
 from uuid import uuid4
-
 from timeit import default_timer as timer
-
 import random
-
 
 def proof_of_work(last_proof):
     """
@@ -19,28 +14,35 @@ def proof_of_work(last_proof):
     - p is the previous proof, and p' is the new proof
     - Use the same method to generate SHA-256 hashes as the examples in class
     """
-
     start = timer()
 
     print("Searching for next proof")
+
     proof = 0
     #  TODO: Your code here
+    guess = f"{last_proof}".encode()
+
+    guess_hash = hashlib.sha256(guess).hexdigest()
+
+    while valid_proof(guess_hash, proof) is False:
+        proof +=1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
-
 
 def valid_proof(last_hash, proof):
     """
     Validates the Proof:  Multi-ouroborus:  Do the last six characters of
     the hash of the last proof match the first six characters of the hash
     of the new proof?
-
     IE:  last_hash: ...AE9123456, new hash 123456E88...
     """
-
     # TODO: Your code here!
-    pass
+    guess = f"{proof}".encode()
+
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    
+    return guess_hash[:6] == last_hash[-6:]
 
 
 if __name__ == '__main__':
@@ -49,15 +51,12 @@ if __name__ == '__main__':
         node = sys.argv[1]
     else:
         node = "https://lambda-coin.herokuapp.com/api"
-
     coins_mined = 0
-
     # Load or create ID
     f = open("my_id.txt", "r")
     id = f.read()
     print("ID is", id)
     f.close()
-
     if id == 'NONAME\n':
         print("ERROR: You must change your name in `my_id.txt`!")
         exit()
@@ -67,10 +66,8 @@ if __name__ == '__main__':
         r = requests.get(url=node + "/last_proof")
         data = r.json()
         new_proof = proof_of_work(data.get('proof'))
-
         post_data = {"proof": new_proof,
                      "id": id}
-
         r = requests.post(url=node + "/mine", json=post_data)
         data = r.json()
         if data.get('message') == 'New Block Forged':
